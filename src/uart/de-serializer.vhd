ed75465d80d2c49c537b1ced1a816e5599a6dc16
Stephan Proß <s.pross@stud.uni-heidelberg.de>
2022-09-26 14:00:28 +0200

    Fixed de-/serializer. Works w. num_bits signal.

--- src/uart/de-serializer.vhd
+++ src/uart/de-serializer.vhd
@@ -6,7 +6,7 @@
 -- Author     : Stephan Proß  <s.pross@stud.uni-heidelberg.de>
 -- Company    :
 -- Created    : 2022-09-22
--- Last update: 2022-09-22
+-- Last update: 2022-09-26
 -- Platform   :
 -- Standard   : VHDL'93/02
 -------------------------------------------------------------------------------
@@ -21,73 +21,87 @@
 -------------------------------------------------------------------------------
 
 library IEEE;
-  use IEEE.STD_LOGIC_1164.all;
-  use IEEE.NUMERIC_STD.all;
-  use IEEE.MATH_REAL.all;
+  use ieee.std_logic_1164.all;
+  use ieee.numeric_std.all;
+  use ieee.math_real.all;
 
-entity De_Serializer is
-
+entity DE_SERIALIZER is
   generic (
-    NUM_BITS : unsigned);  -- Number of bits of the target register.
-
+    MAX_BYTES : integer -- Number of bytes required for the largest register.
+  );
   port (
-    CLK : in std_logic;
-    RST : in std_logic;
-    D_I : in std_logic_vector (7 downto 0);
-    D_O : out std_logic_vector(7 downto 0):
-    REG_I : in std_logic_vector(num_bits-1 downto 0);
-    REG_O : out std_logic_vector(num_bits-1 downto 0);
-    RUN_I : in std_logic;
-    DONE_O : out std_logic);
-end entity De_Serializer;
+    CLK      : in    std_logic;
+    RST      : in    std_logic;
+    NUM_BITS : unsigned(7 downto 0);  -- Number of bits of the target register.
+    D_I      : in    std_logic_vector(7 downto 0);
+    REG_I    : in    std_logic_vector(8 * MAX_BYTES - 1 downto 0);
+    D_O      : out   std_logic_vector(7 downto 0);
+    REG_O    : out   std_logic_vector(8 * MAX_BYTES - 1 downto 0);
+    RUN_I    : in    std_logic;
+    DONE_O   : out   std_logic
+  );
+end entity DE_SERIALIZER;
 
-architecture BEHAVIORAL of De_Serializer is
-  signal byte_count : unsigned range 0 to 2**(integer(ceil(log2(NUM_BITS))))-1;
+architecture BEHAVIORAL of DE_SERIALIZER is
+
+  signal byte_count : integer range 0 to MAX_BYTES;
+  signal done       : std_logic;
 
 begin  -- architecture BEHAVIORAL
 
+  DONE_O <= done;
 
-  -- Counter which only runs, if serialization is not done
-  -- and RUN is high.
+  -- Counter process. If if the number of bytes serialized is lower than the number
+  -- of bits, increase the count each cycle if counter is signaled to run.
+  -- Otherwise, if the count of bytes exceeds the number of serialized bits, set
+  -- done to high and wait until run is set to low to reset the counter.
   COUNTER : process (CLK) is
   begin
-    if rising_edge(CLK) then
-      if (RST = '1' or DONE = '1') then
-        byte_count = 0;
-      else
-        if (RUN ='1') then
-          byte_count <= byte_count +1;
-        end if;
-      end if;
-    end if;
-  end process;
 
-  MAIN : process (CLK) is
-  begin
     if rising_edge(CLK) then
       if (RST = '1') then
-
-        D_O <= (others => '0');
-        REG_O <= (others => '0');
-        DONE <= '0';
+        byte_count <= 0;
+        done  <= '0';
       else
-
-        if (byte_count < (NUM_BITS / 8)) then
-          D_O <= REG_I(8 * (byte_count + 1) - 1 downto 8 * byte_count);
-          REG_O(8 * (byte_count + 1) - 1 downto 8 * byte_count) <= D_I;
-        elsif (byte_count = (NUM_BITS / 8) and NUM_BITS mod 8 > 0) then
-          -- Handle remainder:
-          -- Fill leading bits with zero.
-          D_O(7 downto NUM_BITS mod 8) <= (others => '0');
-          -- Put remainder of the register in the lower bits.
-          D_O((NUM_BITS mod 8) - 1 downto 0) <= REG_I(NUM_BITS - 1 downto 8 * byte_count);
-          REG_O(byte_count - 1 downto 8 * byte_count) <= D_I( NUM_BITS mod 8 -1 downto 0 );
+        -- Are we done serializing?
+        if (to_unsigned(8*byte_count,NUM_BITS'length) < NUM_BITS) then
+          -- If not, set done to low ...
+          done <= '0';
+          -- and increment the counter if run is high.
+          if ( RUN_I = '1' ) then
+            byte_count <= byte_count + 1;
+          end if;
         else
-          DONE <= '1'
+          -- If we are done serializing set done to high and wait until run is
+          -- set to low as ack.
+          if ( RUN_I = '1' ) then
+            done <= '1';
+          else
+            byte_count <= 0;
+            done <= '0';
+          end if;
         end if;
       end if;
     end if;
-  end process;
 
+  end process COUNTER;
+
+  -- Serialize REG_I into D_O and deserialize D_I into REG_O.
+  MAIN : process (CLK) is
+  begin
+
+    if rising_edge(CLK) then
+      if (RST = '1') then
+        D_O   <= (others => '0');
+        REG_O <= (others => '0');
+      else
+        if (to_unsigned(8 * (byte_count), NUM_BITS'length) < NUM_BITS) then
+          D_O                                                   <= REG_I(8 * (byte_count + 1) - 1 downto 8 * byte_count);
+          REG_O(8 * (byte_count + 1) - 1 downto 8 * byte_count) <= D_I;
+        end if;
+      end if;
+    end if;
+
+  end process MAIN;
 
 end architecture BEHAVIORAL;
