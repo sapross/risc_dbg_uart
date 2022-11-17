85ddc72ccc22c856189198ae152cc1d4b8f29a98
Stephan Proß <s.pross@stud.uni-heidelberg.de>
2022-11-17 17:24:15 +0100

    Translated and improved de-serializer.

--- tb/uart/TB_DE-SERIALIZER.vhd
+++ tb/uart/TB_DE-SERIALIZER.vhd
@@ -23,22 +23,27 @@ end entity TB_DE_SERIALIZER;
 
 architecture TB of TB_DE_SERIALIZER is
 
-  constant CLK_RATE      : integer := 100 * 10 ** 6;  -- Hz
-  constant CLK_PERIOD    : time    := 10 ns;          -- ns;
+  constant CLK_RATE                   : integer := 100 * 10 ** 6;  -- Hz
+  constant CLK_PERIOD                 : time    := 10 ns;          -- ns;
 
-  constant MAX_BYTES     : integer := 4;
+  constant MAX_BYTES                  : integer := 4;
 
-  signal clk             : std_logic;
-  signal rst             : std_logic;
+  signal clk                          : std_logic;
+  signal rst,       rst_n             : std_logic;
 
-  signal num_bits        : unsigned( 7 downto 0);
-  signal reg_i,  reg_o   : std_logic_vector(8 * MAX_BYTES - 1 downto 0);
-  signal data_i, data_o  : std_logic_vector(7 downto 0);
-  signal run_0           : std_logic;
-  signal run_1           : std_logic;
-  signal done            : std_logic;
+  signal num_bits                     : unsigned( 7 downto 0);
+  signal reg_i,     reg_o             : std_logic_vector(8 * MAX_BYTES - 1 downto 0);
+  signal data_i,    data_o            : std_logic_vector(7 downto 0);
+  signal run                          : std_logic;
+  signal done                         : std_logic;
+  signal dummy_reg                    : std_logic_vector(8 * MAX_BYTES - 1 downto 0);
+  signal dummy_data                   : std_logic_vector(7 downto 0);
+  signal dummy_done                   : std_logic;
+  signal valid_ser, valid_deser       : std_logic;
 
 begin
+
+  rst_n <= not rst;
 
   -- Plug serializer entity into deserializer.
   SERIALIZER_1 : entity work.de_serializer
@@ -46,14 +51,15 @@ begin
       MAX_BYTES => MAX_BYTES
     )
     port map (
-      CLK        => clk,
-      RST        => rst,
+      CLK_I      => clk,
+      RST_NI     => rst_n,
       NUM_BITS_I => num_bits,
-      D_I        => X"00",
-      D_O        => data_o,
+      BYTE_I     => X"00",
+      BYTE_O     => data_o,
       REG_I      => reg_i,
-      REG_O      => open,
-      RUN_I      => run_0,
+      REG_O      => dummy_reg,
+      RUN_I      => run,
+      VALID_O    => valid_ser,
       DONE_O     => done
     );
 
@@ -62,15 +68,16 @@ begin
       MAX_BYTES => MAX_BYTES
     )
     port map (
-      CLK        => clk,
-      RST        => rst,
+      CLK_I      => clk,
+      RST_NI     => rst_n,
       NUM_BITS_I => num_bits,
-      D_I        => data_o,
-      D_O        => open,
+      BYTE_I     => data_o,
+      BYTE_O     => dummy_data,
       REG_I      => (others => '0'),
       REG_O      => reg_o,
-      RUN_I      => run_1,
-      DONE_O     => open
+      RUN_I      => valid_ser,
+      VALID_O    => valid_deser,
+      DONE_O     => dummy_done
     );
 
   CLK_PROCESS : process is
@@ -83,38 +90,24 @@ begin
 
   end process CLK_PROCESS;
 
-  RUN_DELAY : process is
-  begin
-
-    while (true) loop
-
-      -- Delay the second run signal by one cycle.
-      -- Only required as we plug one serializer into a deserializer.
-      run_1 <= run_0;
-      wait for CLK_PERIOD;
-
-    end loop;
-
-  end process RUN_DELAY;
-
   MAIN : process is
   begin
 
-    rst   <= '1';
-    run_0 <= '0';
+    rst <= '1';
+    run <= '0';
     wait for CLK_PERIOD;
-    rst   <= '0';
+    rst <= '0';
     -- Start running the de-/serializers.
-    run_0    <= '1';
+    run      <= '1';
     reg_i    <= X"000ABCDE";
     num_bits <= to_unsigned(20, num_bits'length);
     -- -- Check whether interrupting the de-/serializers has an effect on the results.
-    -- run_0 <= '0';
+    -- run <= '0';
     -- wait for 2 * clk_period;
-    -- run_0 <= '1'
+    -- run <= '1'
     -- 20 Bits should take ceil(20/8) + 1 cycles.
     wait for ( 3 + 1) * CLK_PERIOD;
-    run_0 <= '0';
+    run <= '0';
     assert done = '1';
     assert reg_i = reg_o;
     rst <= '1';
@@ -122,16 +115,16 @@ begin
     rst <= '0';
     assert done = '0';
     -- Second run with 24 bits and break.
-    run_0    <= '1';
+    run      <= '1';
     reg_i    <= X"00FEDCBA";
     num_bits <= to_unsigned(24, num_bits'length);
     -- Check whether interrupting the de-/serializers has an effect on the results.
-    run_0 <= '0';
+    run <= '0';
     wait for 2 * CLK_PERIOD;
-    run_0 <= '1';
+    run <= '1';
     -- 24 Bits should take ceil(24/8) = 3 and  1 cycle.
     wait for ( 3 + 1) * CLK_PERIOD;
-    run_0 <= '0';
+    run <= '0';
     assert done = '1';
     assert reg_i = reg_o;
     wait for CLK_PERIOD;
